45d8b0dd63e7e12119c22e8047237ff101b2f79d
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2022-01-15 19:36:09 +0600

    для изменения цвета на наведение

--- src/button.py
+++ src/button.py
@@ -1,5 +1,6 @@
 class Button():
-    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+    def __init__(self, image, pos, text_input, font, base_color,
+                 hovering_color):
         self.image = image
         self.x_pos = pos[0]
         self.y_pos = pos[1]
@@ -22,3 +23,12 @@ class Button():
                 position[1] in range(self.rect.top, self.rect.bottom):
             return True
         return False
+
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and \
+                position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input,
+                                         True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input,
+                                         True, self.base_color)
