6e9dc29a639b94e01f2e578dc114a11255a4feab
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2022-01-15 19:29:58 +0600

    красивое меню для нашей игры

---
+++ src/menu.py
@@ -0,0 +1,15 @@
+import pygame, sys
+from button import Button
+
+pygame.init()
+
+pygame.mixer.music.load('rcc/sound_effects/music.mp3')
+pygame.mixer.music.play(-1)
+
+MUSIC_PAUSE = False
+
+SCREEN = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("rcc/images/Background.png")
+BG = pygame.transform.scale(BG, (1920, 1080))
