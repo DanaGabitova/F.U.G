317e17bd36850e8b3c82ea247a54f73b633fd82c
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2022-01-15 19:47:20 +0600

    добавили функцию option

--- src/menu.py
+++ src/menu.py
@@ -48,6 +48,36 @@ def play():
                         pygame.mixer.music.pause()
                     else:
                         pygame.mixer.music.unpause()
-                    main_menu()
+                    main_menu()  # основная функция которая будет содержать в
+                    # себе основной функционал play, options
+
+        pygame.display.update()
+
+
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True,
+                                           "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75),
+                              base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()  # добавим после размышлений
 
         pygame.display.update()
