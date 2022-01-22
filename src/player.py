145f44b6e2b2c5c4204939973bc2f416a9a4809a
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-01-22 23:17:22 +0300

    инициализация

--- src/player.py
+++ src/player.py
@@ -8,3 +8,16 @@ class Player:
         self.x = x
         self.y = y
         self.player_rect = pygame.Rect(self.x, self.y, 10, 16)
+        self.moving_left = False
+        self.moving_right = False
+        self.movement_speed = movement_speed
+        self.scroll = [0, 0]
+        self.vertical_momentum = 0
+        self.air_timer = 0
+        self.current_animation = 0
+        self.animations = [idle_imgs, run_imgs, jump_img]
+        self.animation_index = 0
+        self.flipped = False
+        self.rotation = 0
+        self.jumping = False
+        self.scale = [18, 27]
