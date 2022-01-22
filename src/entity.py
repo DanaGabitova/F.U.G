82525cae178ba1882ec9c0c97ffba6cc13bc0e8e
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-01-22 21:26:25 +0300

    Доработали и добавили отрисовку для класса

--- src/entity.py
+++ src/entity.py
@@ -11,3 +11,21 @@ class Entity:
             self.image = image.copy()
         except:
             self.image = None
+        self.alpha = alpha 
+        self.function = function 
+        self.vector = vector 
+        self.radius = random.randrange(3, 5) 
+        self.sine = random.randrange(-100, 100) 
+ 
+    def draw(self, display, shadows, scroll): 
+        self.sine += 1 
+        self.function(display, self, shadows, scroll, int(np.sin(self.sine))) 
+        self.x += self.vector[0] 
+        self.y += self.vector[1] 
+        try: 
+            self.image.set_alpha(self.alpha) 
+            display.blit(self.image, (self.x - scroll[0],self.y - scroll[1])) 
+        except: 
+            pygame.draw.circle(display, self.color, 
+                               ((self.x - scroll[0]), (self.y - scroll[1])), 
+                               self.radius)
