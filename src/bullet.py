035b716b11474fd24f5c84191934e5bdbe6ba5e3
Coach <95095309+Fixsbreaker@users.noreply.github.com>
2022-01-28 22:21:17 +0600

    pep 8

--- src/bullet.py
+++ src/bullet.py
@@ -8,10 +8,10 @@ class Bullet:
     def __init__(self, x, y, mouse_x, mouse_y, scroll, image, rot_angle):
         self.x = x
         self.y = y
-        self.angle = math.atan2((self.y)-scroll[1]-mouse_y,
-                                (self.x)-scroll[0]-mouse_x)
-        self.x_vel = math.cos(self.angle)*2
-        self.y_vel = math.sin(self.angle)*2
+        self.angle = math.atan2((self.y) - scroll[1] - mouse_y,
+                                (self.x) - scroll[0] - mouse_x)
+        self.x_vel = math.cos(self.angle) * 2
+        self.y_vel = math.sin(self.angle) * 2
         self.image = image
         self.rot_angle = rot_angle
         self.y_vel = 3
