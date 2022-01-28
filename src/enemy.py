228434c6942a8d1dfab9639ef6a3af78f1702a10
Coach <95095309+Fixsbreaker@users.noreply.github.com>
2022-01-28 21:52:08 +0600

    заполнил

--- src/enemy.py
+++ src/enemy.py
@@ -48,6 +48,14 @@ class FlyingEnemy(Enemy):
         display.blit(skeleton_imgs[self.animation_count // 5],
                      (self.x - scroll[0], self.y - scroll[1]))
 
+class FlyingDestoryer(Enemy):
+    def __init__(self, x, y, name):
+        super().__init__(x, y, name)
+        self.speed = random.randrange(1, 3)
+        self.offset = [random.randrange(-50, 50), random.randrange(-50, 50)]
+        self.bullet_cooldown = 0
+        self.animation_count = 0
+        self.width = 1       
 
 
 
