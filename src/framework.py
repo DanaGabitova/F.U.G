f2fd5a0f934386939af8e55e11ae302e2a2f8c99
Coach <95095309+Fixsbreaker@users.noreply.github.com>
2022-01-29 01:28:09 +0600

    будущий фреймворк

--- src/framework.py
+++ src/framework.py
@@ -5,3 +5,17 @@ import time
 import numpy as np
 from rcc.maps import *
 from rcc.images import *
+
+pygame.font.init()
+particles = []
+entities = []
+
+
+def load_map(map_name):
+    blocks = []
+    with open(map_name, 'r') as a_file:
+        for line in a_file:
+            stripped_line = line.strip()
+            stripped_line = stripped_line.split(' ')
+            blocks.append(stripped_line)
+        a_file.close()
