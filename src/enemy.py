894e574e1893e58c0cd1340e3ca1c77a0d630f57
Coach <95095309+Fixsbreaker@users.noreply.github.com>
2022-01-28 17:59:58 +0600

    добавили класс летающего врага

--- src/enemy.py
+++ src/enemy.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 import math
-from rcc.font import *
 from rcc.images import *
 
 class Enemy:
@@ -10,6 +9,14 @@ class Enemy:
         self.y = y
         self.name = name
         self.rect = None
+
+class FlyingEnemy(Enemy):
+    def __init__(self, x, y, name):
+        super().__init__(x, y, name)
+        self.speed = random.randrange(1, 3)
+        self.offset = [random.randrange(-50, 50), random.randrange(-50, 50)]
+        self.bullet_cooldown = 0
+        self.animation_count = 0 
 
 
 
