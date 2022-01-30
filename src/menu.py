605cf221698d0ff1c49f7517821d6add77b4ef11
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-01-30 09:01:54 +0600

    запуск игры с меню

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from button import Button
+from main import main
 
 pygame.init()
 
@@ -17,42 +18,6 @@ BG = pygame.transform.scale(BG, (1920, 1080))
 
 def get_font(size):
     return pygame.font.Font("rcc/font/font.ttf", size)
-
-def play():
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill('black')
-
-
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True,
-                                        "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(600, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75),
-                           base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    MUSIC_PAUSE = not MUSIC_PAUSE
-                    if MUSIC_PAUSE:
-                        pygame.mixer.music.pause()
-                    else:
-                        pygame.mixer.music.unpause()
-                    main_menu()  # основная функция которая будет содержать в
-                    # себе основной функционал play, options
-
-        pygame.display.update()
-
 
 def options():
     while True:
@@ -119,7 +84,8 @@ def main_menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    play()
+                    main()
+                    return
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
